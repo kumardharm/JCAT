b8990f13441741a8acf776ac58c669214ba26694
jkomal10 <komalv.j45@gmail.com>
2019-01-21 14:52:58 +0530

    pushing log

--- JCAT_API/src/main/java/com/cg/jcat/api/JcatApiApplication.java
+++ JCAT_API/src/main/java/com/cg/jcat/api/JcatApiApplication.java
@@ -1,13 +1,21 @@
 package com.cg.jcat.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JcatApiApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(JcatApiApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JcatApiApplication.class, args);
+//		logger.debug("--debug--Application Started--");
+//		logger.trace("--trace--Application Started--");
+//		logger.error("--error--Application Started--");
+	logger.info("--info--Application Started--");
+//		logger.warn("--warn--Application Started--");
+		
 	}
 
 }
