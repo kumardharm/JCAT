9472d5538c46c64e5724957a73cbaa9529ea0730
jkomal10 <komal.a.jagtap@capgemini.com>
2019-01-25 11:11:18 +0530

    changes

--- JCAT_API/src/main/java/com/cg/jcat/api/service/IDTMigrationRuleService.java
+++ JCAT_API/src/main/java/com/cg/jcat/api/service/IDTMigrationRuleService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.cg.jcat.api.dao.DTMigrationModel;
 import com.cg.jcat.api.dao.DTMigrationRuleModel;
 import com.cg.jcat.api.exception.JcatExceptions;
+import com.cg.jcat.api.exception.OptionTextNotNullException;
 import com.cg.jcat.api.exception.SystemExceptions;
 
 @Service
@@ -14,7 +15,7 @@ public interface IDTMigrationRuleService {
 	
 	public List<DTMigrationRuleModel> getMigrationRule(int migrationId) throws JcatExceptions;
 	
-	public boolean saveMigrationRule(List<DTMigrationRuleModel> dtMigrationRuleModel)  throws SystemExceptions;
+	public boolean saveMigrationRule(List<DTMigrationRuleModel> dtMigrationRuleModel)  throws SystemExceptions, OptionTextNotNullException;
 	
 	public List<DTMigrationModel> getMigrationPattern();
 
